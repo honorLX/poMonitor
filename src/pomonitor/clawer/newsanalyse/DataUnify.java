6447e228fe1894a267ce59a2b458b621d73dc644
市委 <@wsw>
2015-12-15 22:34:13 +0800

    规范日期

---
+++ src/pomonitor/clawer/newsanalyse/DataUnify.java
@@ -0,0 +1,27 @@
+package pomonitor.clawer.newsanalyse;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class DataUnify {
+	public static String DataUn(String dates) {
+		String datatime = null;
+		String datatimes = null;
+		String reExpression = "[0-9]{4}\\-[0-9]+\\-[0-9]+";
+		Pattern p = Pattern.compile(reExpression);
+		Matcher date = p.matcher(dates);
+		while (date.find()) {
+			datatime = date.group();
+
+		}
+		String[] Idatatime = datatime.split("-");
+		if (Idatatime[1].length() < 2) {
+			Idatatime[1] = "0" + Idatatime[1];
+		}
+		if (Idatatime[2].length() < 2) {
+			Idatatime[2] = "0" + Idatatime[2];
+		}
+		datatimes = Idatatime[0] + "-" + Idatatime[1] + "-" + Idatatime[2];
+		return datatimes;
+	}
+}
