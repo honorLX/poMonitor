0d1cb5b698caa385cbc4d24b1a9c5be5808ef0b4
herozhao <1014187702@qq.com>
2015-12-16 11:06:03 +0800

    添加了几个数据库映射，添加了fastjson包

---
+++ src/pomonitor/entity/Sensword.java
@@ -0,0 +1,80 @@
+package pomonitor.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * Sensword entity. @author MyEclipse Persistence Tools
+ */
+@Entity
+@Table(name = "sensword", catalog = "pomonitor")
+public class Sensword implements java.io.Serializable {
+
+	// Fields
+
+	private Integer sensid;
+	private User user;
+	private String senslevel;
+	private String sensvalue;
+
+	// Constructors
+
+	/** default constructor */
+	public Sensword() {
+	}
+
+	/** full constructor */
+	public Sensword(User user, String senslevel, String sensvalue) {
+		this.user = user;
+		this.senslevel = senslevel;
+		this.sensvalue = sensvalue;
+	}
+
+	// Property accessors
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "sensid", unique = true, nullable = false)
+	public Integer getSensid() {
+		return this.sensid;
+	}
+
+	public void setSensid(Integer sensid) {
+		this.sensid = sensid;
+	}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "userid")
+	public User getUser() {
+		return this.user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Column(name = "senslevel")
+	public String getSenslevel() {
+		return this.senslevel;
+	}
+
+	public void setSenslevel(String senslevel) {
+		this.senslevel = senslevel;
+	}
+
+	@Column(name = "sensvalue")
+	public String getSensvalue() {
+		return this.sensvalue;
+	}
+
+	public void setSensvalue(String sensvalue) {
+		this.sensvalue = sensvalue;
+	}
+
+}
