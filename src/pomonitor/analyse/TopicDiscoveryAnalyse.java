063a01184aab15f36d6c8b8b6ee0d246cb3bbe50
hengyi <hengyicai@hotmail.com>
2015-12-17 23:04:44 +0800

    完成数据获取并组装为 话题发现模块需要的 文章列表

--- src/pomonitor/analyse/TopicDiscoveryAnalyse.java
+++ src/pomonitor/analyse/TopicDiscoveryAnalyse.java
@@ -1,9 +1,17 @@
 package pomonitor.analyse;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
+import pomonitor.analyse.entity.TDArticle;
+import pomonitor.analyse.entity.TDArticleTerm;
+import pomonitor.analyse.entity.TDPosition;
 import pomonitor.analyse.entity.Topic;
+import pomonitor.analyse.segment.TermsGenerator;
+import pomonitor.entity.News;
+import pomonitor.entity.NewsDAO;
+
+import com.hankcs.hanlp.seg.common.Term;
 
 /**
  * 话题发现模块, 介于Controller与具体的话题发现模块之间;向上对controller服务; 向下调用话题发现具体分析包提供的功能
@@ -12,11 +20,42 @@ import pomonitor.analyse.entity.Topic;
  */
 public class TopicDiscoveryAnalyse {
 
-	public List<Topic> DiscoverTopics(Date startDate, Date endDate, int userId) {
+	public List<Topic> DiscoverTopics(String startDateStr, String endDateStr,
+			int userId) {
 		// 根据起止时间获取数据库中的新闻文本
-
+		NewsDAO nd = new NewsDAO();
+		List<News> newsList = nd.findBetweenDate(startDateStr, endDateStr);
 		// 作分词，过滤预处理
+		List<TDArticle> tdArticleList = new ArrayList<TDArticle>();
+		TermsGenerator generateTerms = new TermsGenerator();
+		for (News news : newsList) {
+			TDArticle tmpArt = new TDArticle();
+			List<TDArticleTerm> tmpTDArtTerms = new ArrayList<TDArticleTerm>();
 
+			List<Term> tmpTermList_allcontent = generateTerms.getTerms(news
+					.getAllContent());
+			List<Term> tmpTermList_keyword = generateTerms.getTerms(news
+					.getKeyWords());
+			for (Term term : tmpTermList_allcontent) {
+				TDArticleTerm tmpArtTerm = new TDArticleTerm();
+				tmpArtTerm.setposition(TDPosition.BODY);
+				tmpArtTerm.setvalue(term.word);
+				tmpTDArtTerms.add(tmpArtTerm);
+			}
+			for (Term term : tmpTermList_keyword) {
+				TDArticleTerm tmpArtTerm = new TDArticleTerm();
+				tmpArtTerm.setposition(TDPosition.META);
+				tmpArtTerm.setvalue(term.word);
+				tmpTDArtTerms.add(tmpArtTerm);
+			}
+			tmpArt.setArticleTerms(tmpTDArtTerms);
+			tmpArt.setComeFrom(news.getWeb());
+			tmpArt.setDescription(news.getContent());
+			tmpArt.setTimestamp(news.getTime());
+			tmpArt.setTitle(news.getTitle());
+			tmpArt.setUrl(news.getUrl());
+			tdArticleList.add(tmpArt);
+		}
 		// 调用话题发现功能模块，返回话题集合
 
 		return null;
--- src/pomonitor/test/TestUserAndSensword.java
+++ src/pomonitor/test/TestUserAndSensword.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import pomonitor.entity.EntityManagerHelper;
+import pomonitor.entity.News;
+import pomonitor.entity.NewsDAO;
 import pomonitor.entity.Sensword;
 import pomonitor.entity.SenswordDAO;
 import pomonitor.entity.User;
@@ -12,28 +14,8 @@ import pomonitor.entity.UserDAO;
 public class TestUserAndSensword {
 
 	public static void main(String[] args) {
-		UserDAO ud = new UserDAO();
-		User root = ud.findById(1);
-
-		// 添加敏感词
-		String[] list_A = new String[] { "贪腐", "南华大学", "黑幕", "跳楼", "压迫", "反抗",
-				"喜讯", "获奖" };
-		String[] list_B = new String[] { "垃圾", "脏", "乱", "差", "坑" };
-		String[] list_C = new String[] { "红包", "女学生" };
-		List<String[]> lists = new ArrayList<String[]>();
-		lists.add(list_A);
-		lists.add(list_B);
-		lists.add(list_C);
-		SenswordDAO sd = new SenswordDAO();
-		EntityManagerHelper.beginTransaction();
-
-		for (int i = 0; i < 3; i++) {
-			for (String string : lists.get(i)) {
-				Sensword sens = new Sensword((3 - i) + "", string,
-						root.getUserid());
-				sd.save(sens);
-			}
-		}
-		EntityManagerHelper.commit();
+		NewsDAO nd = new NewsDAO();
+		List<News> newsList = nd.findBetweenDate("2009-07-20", "2011-02-20");
+		System.out.println(newsList.size());
 	}
 }
