348797a70a8ee3b47edd0fa17e5b980393fd46b4
xyl576807077 <xyl576807077@gmail.com>
2016-01-07 22:01:52 +0800

    重写SitEvaluation，然而数据库那里还有一些小的编码问题，如需测试请给定开始结束日期都为2002-02-20

--- src/pomonitor/entity/NewsTend.java
+++ src/pomonitor/entity/NewsTend.java
@@ -1,107 +1,126 @@
 package pomonitor.entity;
-
 // default package
 
-import static javax.persistence.GenerationType.IDENTITY;
-
 import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+
 
 /**
- * 用于存储新闻倾向性的计算结果 的实体 NewsTend entity. @author MyEclipse Persistence Tools
+ * NewsTend entity. @author MyEclipse Persistence Tools
  */
 @Entity
-@Table(name = "newstend", catalog = "pomonitor")
-public class NewsTend implements java.io.Serializable {
+@Table(name="newstend"
+    ,catalog="pomonitor"
+, uniqueConstraints = @UniqueConstraint(columnNames="newsId")
+)
 
-	// Fields
+public class NewsTend  implements java.io.Serializable {
 
-	private Integer id;
-	private Integer newsId;
-	private String web;
-	private Date date;
-	private Integer tendclass;
-	private Float tendscore;
 
-	// Constructors
+    // Fields    
 
-	/** default constructor */
-	public NewsTend() {
-	}
+     private Integer id;
+     private Integer newsId;
+     private String web;
+     private Date date;
+     private Integer tendclass;
+     private Float tendscore;
 
-	/** full constructor */
-	public NewsTend(Integer newsId, String web, Date date, Integer tendclass,
-			Float tendscore) {
-		this.newsId = newsId;
-		this.web = web;
-		this.date = date;
-		this.tendclass = tendclass;
-		this.tendscore = tendscore;
-	}
 
-	// Property accessors
-	@Id
-	@GeneratedValue(strategy = IDENTITY)
-	@Column(name = "id", unique = true, nullable = false)
-	public Integer getId() {
-		return this.id;
-	}
+    // Constructors
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    /** default constructor */
+    public NewsTend() {
+    }
 
-	@Column(name = "newsId", nullable = false)
-	public Integer getNewsId() {
-		return this.newsId;
-	}
+    
+    /** full constructor */
+    public NewsTend(Integer newsId, String web, Date date, Integer tendclass, Float tendscore) {
+        this.newsId = newsId;
+        this.web = web;
+        this.date = date;
+        this.tendclass = tendclass;
+        this.tendscore = tendscore;
+    }
 
-	public void setNewsId(Integer newsId) {
-		this.newsId = newsId;
-	}
+   
+    // Property accessors
+    @Id @GeneratedValue(strategy=IDENTITY)
+    
+    @Column(name="id", unique=true, nullable=false)
 
-	@Column(name = "web", nullable = false)
-	public String getWeb() {
-		return this.web;
-	}
+    public Integer getId() {
+        return this.id;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    @Column(name="newsId", unique=true, nullable=false)
 
-	public void setWeb(String web) {
-		this.web = web;
-	}
+    public Integer getNewsId() {
+        return this.newsId;
+    }
+    
+    public void setNewsId(Integer newsId) {
+        this.newsId = newsId;
+    }
+    
+    @Column(name="web", nullable=false)
 
-	@Temporal(TemporalType.DATE)
-	@Column(name = "date", nullable = false, length = 10)
-	public Date getDate() {
-		return this.date;
-	}
+    public String getWeb() {
+        return this.web;
+    }
+    
+    public void setWeb(String web) {
+        this.web = web;
+    }
+    @Temporal(TemporalType.DATE)
+    @Column(name="date", nullable=false, length=10)
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
+    public Date getDate() {
+        return this.date;
+    }
+    
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    @Column(name="tendclass", nullable=false)
 
-	@Column(name = "tendclass", nullable = false)
-	public Integer getTendclass() {
-		return this.tendclass;
-	}
+    public Integer getTendclass() {
+        return this.tendclass;
+    }
+    
+    public void setTendclass(Integer tendclass) {
+        this.tendclass = tendclass;
+    }
+    
+    @Column(name="tendscore", nullable=false, precision=12, scale=0)
 
-	public void setTendclass(Integer tendclass) {
-		this.tendclass = tendclass;
-	}
+    public Float getTendscore() {
+        return this.tendscore;
+    }
+    
+    public void setTendscore(Float tendscore) {
+        this.tendscore = tendscore;
+    }
+   
 
-	@Column(name = "tendscore", nullable = false, precision = 12, scale = 0)
-	public Float getTendscore() {
-		return this.tendscore;
-	}
 
-	public void setTendscore(Float tendscore) {
-		this.tendscore = tendscore;
-	}
+
+
+
+
+
 
 }
