cefaa5372daede6e330fdfbbb5d37814ad760150
xyl576807077 <xyl576807077@gmail.com>
2016-01-17 00:01:19 +0800

    再次修改网页评测功能生成的Json的格式

--- src/pomonitor/analyse/ArticleTendAnalyse.java
+++ src/pomonitor/analyse/ArticleTendAnalyse.java
@@ -11,6 +11,7 @@ import javax.json.Json;
 import org.eclipse.persistence.sessions.serializers.JSONSerializer;
 
 import com.alibaba.fastjson.JSON;
+import com.hankcs.hanlp.collection.trie.bintrie.BaseNode.Status;
 
 import pomonitor.analyse.articletend.ArticleTendAnalyseRealize;
 import pomonitor.analyse.entity.TendAnalyseArticle;
@@ -134,6 +135,7 @@ public class ArticleTendAnalyse {
 		HashMap<String, WebScore> hashMap = showWebTend(start_time, end_time,
 				UserId);
 		Result result = new Result();
+		Test test = new Test();
 		for (Iterator iterator = hashMap.keySet().iterator(); iterator
 				.hasNext();) {
 			String webName = (String) iterator.next();
@@ -144,69 +146,12 @@ public class ArticleTendAnalyse {
 			result.series[1].data.add(webScore.obj * 1.0 / all);
 			result.series[2].data.add(webScore.neg * 1.0 / all);
 		}
-		resJson = JSON.toJSONString(result);
+		test.messag = "Query Success";
+		test.status = 0;
+		test.result = result;
+		resJson = JSON.toJSONString(test);
 		return resJson;
 	}
-
-	// public static byte[] gbk2utf8(String chenese) {
-	//
-	// // Step 1: 得到GBK编码下的字符数组，一个中文字符对应这里的一个c[i]
-	// char c[] = chenese.toCharArray();
-	//
-	// // Step 2: UTF-8使用3个字节存放一个中文字符，所以长度必须为字符的3倍
-	// byte[] fullByte = new byte[3 * c.length];
-	//
-	// // Step 3: 循环将字符的GBK编码转换成UTF-8编码
-	// for (int i = 0; i < c.length; i++) {
-	//
-	// // Step 3-1：将字符的ASCII编码转换成2进制值
-	// int m = (int) c[i];
-	// String word = Integer.toBinaryString(m);
-	// // System.out.println(word);
-	//
-	// // Step 3-2：将2进制值补足16位(2个字节的长度)
-	// StringBuffer sb = new StringBuffer();
-	// int len = 16 - word.length();
-	// for (int j = 0; j < len; j++) {
-	// sb.append("0");
-	// }
-	// // Step 3-3：得到该字符最终的2进制GBK编码
-	// // 形似：1000 0010 0111 1010
-	// sb.append(word);
-	//
-	// // Step 3-4：最关键的步骤，根据UTF-8的汉字编码规则，首字节
-	// // 以1110开头，次字节以10开头，第3字节以10开头。在原始的2进制
-	// // 字符串中插入标志位。最终的长度从16--->16+3+2+2=24。
-	// sb.insert(0, "1110");
-	// sb.insert(8, "10");
-	// sb.insert(16, "10");
-	// // System.out.println(sb.toString());
-	//
-	// // Step 3-5：将新的字符串进行分段截取，截为3个字节
-	// String s1 = sb.substring(0, 8);
-	// String s2 = sb.substring(8, 16);
-	// String s3 = sb.substring(16);
-	//
-	// // Step 3-6：最后的步骤，把代表3个字节的字符串按2进制的方式
-	// // 进行转换，变成2进制的整数，再转换成16进制值
-	// byte b0 = Integer.valueOf(s1, 2).byteValue();
-	// byte b1 = Integer.valueOf(s2, 2).byteValue();
-	// byte b2 = Integer.valueOf(s3, 2).byteValue();
-	//
-	// // Step 3-7：把转换后的3个字节按顺序存放到字节数组的对应位置
-	// byte[] bf = new byte[3];
-	// bf[0] = b0;
-	// bf[1] = b1;
-	// bf[2] = b2;
-	//
-	// fullByte[i * 3] = bf[0];
-	// fullByte[i * 3 + 1] = bf[1];
-	// fullByte[i * 3 + 2] = bf[2];
-	//
-	// // Step 3-8：返回继续解析下一个中文字符
-	// }
-	// return fullByte;
-	// }
 
 	public static String ChineseWebNameToEng(String webname) {
 		switch (webname) {
@@ -332,5 +277,18 @@ public class ArticleTendAnalyse {
 			series[2].name = "贬";
 			xAxis = new ArrayList<>();
 		}
+	}
+
+	class Test {
+		public String messag;
+		public int status;
+		public Result result;
+
+//		public Test(String message, String status, Result result) {
+//			this.messag = message;
+//			this.status = status;
+//			this.result = result;
+//		}
+
 	}
 }
